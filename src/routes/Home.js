68c35aea75e21d064e50a05acf124b0ac46c8c12
Asa Murphy <asa.g.murphy75@gmail.com>
2024-03-01 16:33:21 -0500

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
+import FlightSelection from "./FlightSelection";
 import "./Home.css";
 
 function Home() {
     return (
         <div className="home">
+            <FlightSelection/>
             <h1>Welcome to FlyHi</h1>
             <h2>Login</h2>
 
@@ -27,18 +28,6 @@ function Home() {
                 </div>
             </form>
 
-=======
-import react from "react";
-import airport from "../assets/airport.jpg"
-import FlightSelection from "./FlightSelection";
-function Home() {
-    return (
-        <div className="home">
-            <h1>Home</h1>
-            <FlightSelection/>
-            <img src={airport} alt="" className='airport'/>
->>>>>>> 720ac07b7562150487d444dd1493aa038b7e2db7
-        </div>
     )
 }
 export default Home;
